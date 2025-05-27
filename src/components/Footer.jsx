e36662c2acc11f894e537ccadda24130402a32bb
letitiacowan <letitia.cowan01@hotmail.com>
2025-05-27 19:45:42 +1000

    year fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -67,7 +67,7 @@ export function Footer() {
           </div>
         </div>
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 Yard Dog Workwear. All rights reserved.</p>
+          <p>&copy; 2025 Yard Dog Workwear. All rights reserved.</p>
         </div>
       </div>
     </footer>
